3122177c513a933d005f54c0bacde1b637656c86
Francisco Maldonado <up202004244@up.pt>
2023-03-23 08:57:18 +0000

    merge

---
+++ tp4/MyUnitCubeQuad.js
@@ -0,0 +1,59 @@
+import { CGFobject } from "../lib/CGF.js";
+import { MyQuad } from "./MyQuad.js";
+
+export class MyUnitCubeQuad extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.scene.quad = new MyQuad(this.scene);
+    }
+    display() {
+        
+        //TOP
+        this.scene.pushMatrix();
+        this.scene.translate(0.0, 0.0, 0.5);
+        this.scene.quad.display();
+        this.scene.popMatrix();
+
+        //BOTTOM
+        this.scene.pushMatrix();
+        this.scene.translate(0.0, 0.0, -0.5);
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.scene.quad.display();
+        this.scene.popMatrix();
+
+        //FRONT
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0.0, 0.0);
+        this.scene.rotate(Math.PI/2, 0, 1, 0);
+        this.scene.quad.display();
+        this.scene.popMatrix();
+
+        //BACK
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0.0, 0.0);
+        this.scene.rotate(3*Math.PI/2, 0, 1, 0);
+        this.scene.quad.display();
+        this.scene.popMatrix();
+
+        //RIGHT
+        this.scene.pushMatrix();
+        this.scene.translate(0.0, 0.5, 0.0);
+        this.scene.rotate(-Math.PI/2, 0, 0, 1);
+        this.scene.rotate(-Math.PI/2, 0, 1, 0);
+        this.scene.quad.display();
+        this.scene.popMatrix();
+
+        //LEFT
+        this.scene.pushMatrix();
+        this.scene.translate(0.0, -0.5, 0.0);
+        this.scene.rotate(-Math.PI/2, 0, 0, 1);
+        this.scene.rotate(Math.PI/2, 0, 1, 0);
+        this.scene.quad.display();
+        this.scene.popMatrix();
+        
+    }
+}
+
