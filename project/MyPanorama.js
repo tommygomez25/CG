37478add3f09d173c5802f2c17a142e63506fef0
Francisco Maldonado <up202004244@up.pt>
2023-04-20 08:53:46 +0100

    merge conflicts fix

--- project/MyPanorama.js
+++ project/MyPanorama.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CGFobject, CGFappearance } from '../../lib/CGF.js';
 import { MySphere } from './primitives/MySphere.js'; 
 
@@ -28,35 +27,4 @@ export class MyPanorama extends CGFobject {
     this.panoramSphere.display();
     this.scene.popMatrix();
   }
-=======
-import { CGFobject, CGFappearance } from '../../lib/CGF.js';
-import { MySphere } from './MySphere.js'; 
-
-export class MyPanorama extends CGFobject {
-  
-  constructor(scene, texture) {
-    super(scene);
-    
-    this.texture = texture;
-    this.panoramaAppearance = new CGFappearance(this.scene);
-    this.panoramaAppearance.setTexture(this.texture);
-
-    this.initBuffers();
-  }
-
-  initBuffers() {
-   
-    this.panoramSphere = new MySphere(this.scene, 30, 15, true); 
-  }
-
-  display() {
-    this.scene.pushMatrix();
-    this.panoramaAppearance.apply();
-    this.scene.translate(...this.scene.camera.position);
-    this.scene.scale(200,200,200);
-    
-    this.panoramSphere.display();
-    this.scene.popMatrix();
-  }
->>>>>>> 114ccd77442d27100c585f7492fc7184916f5643
 }
